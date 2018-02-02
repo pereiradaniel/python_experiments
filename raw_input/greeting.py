68f34eddd03e414847101ab9bdb0ea64747829f1
Daniel <truenorthtrainingcompany@gmail.com>
2018-02-02 11:12:51 -0500

    Add info to greeting

--- raw_input/greeting.py
+++ raw_input/greeting.py
@@ -1,6 +1,7 @@
 # Prompt user for name
 name = raw_input("What is your name? :  ")
-if len(name) > 0:
+# Print name if name is greater than 0 and consists of alphabetic characters
+if len(name) > 0 and name.isalpha():
     print name
 else:
     print "invalid"
