f1bc8b08034f7b7e05eeae889ceea8e84c7cdd40
Daniel <truenorthtrainingcompany@gmail.com>
2018-02-04 13:15:45 -0500

    Update greeting.py

--- raw_input/greeting.py
+++ raw_input/greeting.py
@@ -2,7 +2,7 @@
 name = raw_input("What is your name? :  ")
 # Print name if name is greater than 0 and consists of alphabetic characters
 if len(name) > 0 and name.isalpha():
-    print name
+    print "Hello " + name
 else:
     print "invalid"
 
